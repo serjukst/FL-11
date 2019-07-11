faac4e025c57c8b18a52cd39c15cd46d3986646f
Serhii Stepanchuk <51526907+serjukst@users.noreply.github.com>
2019-07-11 11:24:32 +0300

    done reverseNumber

--- FL11_HW8/homework/js/reverseNumber.js
+++ FL11_HW8/homework/js/reverseNumber.js
@@ -1,6 +1,22 @@
-function reverseNumber (number) {
-    return (
-        parseFloat(number.toString().split('').reverse().join('')) * Math.sign(number)
-      )     
+function reverseNumber(n) {
+  let result = '';
+  if (n >= 0) {
+    while (n) {
+      result += (n % 10);
+      result *= 10;
+
+      n = Math.floor(n / 10);
+    }
+  } else {
+    n = n * (-1);
+    while (n) {
+      result += (n % 10);
+      result *= 10;
+
+      n = Math.floor(n / 10);
+    }
+    result = result * (-1);
+  }
+  return result / 10;
 }
-reverseNumber ();
+reverseNumber();
