cffabb367f74c8f8acb42f60e32a0c36753b66f3
Serhii Stepanchuk <51526907+serjukst@users.noreply.github.com>
2019-07-15 18:14:57 +0300

    done 8 task

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,3 +1,5 @@
+//Task 0;
+
 function getNumber(str) {
     let result = [];
     for (let i = 0; i < str.length; i++) {
@@ -7,6 +9,8 @@ function getNumber(str) {
     }
     return result;
 }
+
+//Task 1;
 
 function findTypes() {
     let result = {};
@@ -33,11 +37,15 @@ function findTypes() {
     return result;
 }
 
+//Task 2;
+
 function executeforEach(arr, fn) {
     for (let i = 0; i < arr.length; i++) {
         fn(arr[i]);
     }
 }
+
+//Task 3;
 
 function mapArray(arr, fn) {
     let res = [];
@@ -46,6 +54,8 @@ function mapArray(arr, fn) {
     }
     return res;
 }
+
+//Task 4;
 
 function filterArray(arr, fn) {
     let res = [];
@@ -57,20 +67,74 @@ function filterArray(arr, fn) {
     return res;
 }
 
+//Task 5;
+
 function showFormattedDate(date) {
     let year = date.getFullYear();
     let monthIndex = date.getMonth()
     let dayOfMonth = date.getDate();
-    let formatMonth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    formatDate = formatMonth[monthIndex] + ' ' + dayOfMonth + ' ' + year;
-    result = 'Date: ' + formatDate;
-    return result;
+    let formatMonth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let formatDate = 'Date: ' + formatMonth[monthIndex] + ' ' + dayOfMonth + ' ' + year;
+    return formatDate;
+    /*let options = {
+            month : 'short',
+            day : 'numeric',
+            year : 'numeric'
+        }
+        return 'Date: ' + date.toLocaleString('en-GB', options)
+    */
 }
-/*
-var today = new Date().toLocaleDateString('en-GB', {  
-	day : 'numeric',
-	month : 'short',
-	year : 'numeric'
-})
-https://www.toptal.com/software/definitive-guide-to-datetime-manipulation
-*/
+
+//https://www.toptal.com/software/definitive-guide-to-datetime-manipulation
+
+//Task 6;
+
+function canConvertToDate(date) {
+    let check = new Date(date).toString();
+    if (check === 'Invalid Date') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//Task 7;
+
+function daysBetween(data1, data2) {
+    const msecs = 1000;
+    const seconds = 60;
+    const hours = 24;
+    let convertData1 = data1.getTime();
+    let convertData2 = data2.getTime();
+    let microSecondsDiff = Math.abs(convertData1 - convertData2);
+    let daysDiff = Math.floor(microSecondsDiff / (msecs * seconds * seconds * hours));
+    return daysDiff;
+}
+
+//Task 8;
+
+function getAmountOfAdultPeople(data) {
+    const daysFor18Years = 6573;
+    let nowDate = new Date();
+    let countAdultPeople = 0;
+    for (let i = 0; i < data.length; i++) {
+        let birthDay = new Date(data[i].birthday);
+        let checkDay = daysBetween(nowDate, birthDay);
+        if (checkDay > daysFor18Years) {
+            countAdultPeople += 1;
+        }
+    }
+
+    return countAdultPeople;
+}
+
+//Task 9;
+
+function getKeys(obj) {
+    let result = [];
+    for (prop in obj) {
+        result.push(prop);
+    } 
+    return result;
+
+}
