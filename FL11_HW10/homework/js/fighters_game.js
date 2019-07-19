be623f67b3f21fb4ac307399732336f2a4c54e95
Serhii Stepanchuk <51526907+serjukst@users.noreply.github.com>
2019-07-19 13:12:52 +0300

    done

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,25 +1,54 @@
 class Fighter {
     constructor(prop) {
+        const NUM_100 = 100;
+        let countWins = 0;
+        let countLosses = 0;
         this.getName = () => prop.name;
         this.getDamage = () => prop.damage;
         this.getAgility = () => prop.agility;
         this.getHealth = () => prop.hp;
-        this.dealDamage = (num) => {
-            if(prop.hp - num < 0) {
-                prop.hp = 0
-            } else {
-                prop.hp -= num;
-            }
-        }
-        this.atack = (warior2) => {
-            let probability = Math.random();
-            let agilityWarior = this.getAgility()/100;
-            if (probability > agilityWarior) {
+        this.attack = (warior2) => {
+            let agilityWarior = warior2.getAgility(this.getAgility()) / NUM_100;
+            if (Math.random() > agilityWarior) {
                 warior2.dealDamage(this.getDamage());
                 console.log(`${this.getName()} make ${this.getDamage()} damage to ${warior2.getName()}`)
             } else {
                 console.log(`${this.getName()} attacked missed`);
             }
+        };
+        this.logCombatHistory = () => console.log(`Name: ${prop.name}, Wins: ${countWins}, Losses: ${countLosses}`);
+        this.heal = (healthPoint) => {
+            prop.hp + healthPoint > NUM_100 ? prop.hp = NUM_100 : prop.hp += healthPoint;
+        };
+        this.dealDamage = (damagePoint) => {
+            prop.hp - damagePoint < 0 ? prop.hp = 0 : prop.hp -= damagePoint;
+        };
+        this.addWin = () => countWins++;
+        this.addloss = () => countLosses++
+    }
+}
+
+function battle(warior1, warior2) {
+    if (!warior1.getHealth()) {
+        console.log(`${warior1.getName()} is dead and can't fight.`);
+    } else if (!warior2.getHealth()) {
+        console.log(`${warior2.getName()} is dead and can't fight.`);
+    } else {
+        while (warior1.getHealth() > 0 && warior2.getHealth() > 0) {
+            warior1.attack(warior2);
+            warior2.attack(warior1);
+        }
+        console.log(`${warior1.getName()} has ${warior1.getHealth()} health 
+${warior2.getName()} has ${warior2.getHealth()} health`);
+
+        if (warior1.getHealth()) {
+            warior2.addWin();
+            warior1.addloss();
+            console.log(`${warior2.getName()} is winner.Congratulations!`);
+        } else {
+            warior1.addWin();
+            warior2.addloss();
+            console.log(`${warior1.getName()} is winner.Congratulations!`);
         }
     }
 }
