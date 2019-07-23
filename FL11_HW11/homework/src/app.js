f5c23cac32bae1ab0965f2cd1fa3d1cba2353f84
Serhii Stepanchuk <51526907+serjukst@users.noreply.github.com>
2019-07-23 13:05:10 +0300

    fix some bags

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,25 +1,35 @@
 const maxNumberTask = 11;
 let addButton = document.getElementById('add');
-addButton.disabled = true;
 let taskList = document.getElementById('task-list');
 let inputTask = document.getElementById('new-action');
+let dragSrcEl = null;
+
+addButton.disabled = true;
 inputTask.addEventListener('input', () => {
   inputTask.value ? addButton.disabled = false : addButton.disabled = true
 });
 
+addButton.onclick = addTask;
+
 function createNewTask(task) {
   let listItem = document.createElement('li');
   listItem.setAttribute('draggable', true);
+  addDnDHandlers(listItem);
+  
   let checkBox = document.createElement('button')
   checkBox.className = 'material-icons checkbox';
   checkBox.innerHTML = '<i class="material-icons">check_box_outline_blank</i>'
+  
   let label = document.createElement('label');
   label.innerText = task;
+  
   let itemInput = document.createElement('input');
   itemInput.type = 'Text';
+  
   let editBtn = document.createElement('button');
   editBtn.className = 'material-icons edit';
   editBtn.innerHTML = '<i class="material-icons">edit</i>';
+  
   let deleteBtn = document.createElement('button');
   deleteBtn.className = 'material-icons delete';
   deleteBtn.innerHTML = '<i class="material-icons">delete</i>';
@@ -33,6 +43,7 @@ function createNewTask(task) {
   deleteBtn.addEventListener('click', deleteTask);
   editBtn.addEventListener('click', editTask);
   checkBox.addEventListener('click', finishTask);
+
   return listItem;
 }
 
@@ -42,6 +53,7 @@ function addTask() {
     taskList.appendChild(newTask);
     inputTask.value = '';
     addButton.disabled = true;
+
   } else {
     let err = document.getElementById('error');
     err.style = 'display: block';
@@ -51,12 +63,11 @@ function addTask() {
   }
 }
 
-addButton.onclick = addTask;
-
 function deleteTask() {
   let listItem = this.parentNode;
   let ul = listItem.parentNode;
   ul.removeChild(listItem);
+
   let err = document.getElementById('error');
   err.style = 'display: none';
   inputTask.disabled = false;
@@ -68,16 +79,19 @@ function editTask() {
   let label = listItem.querySelector('label');
   let input = listItem.querySelector('input[type="text"');
   let containsClass = listItem.classList.contains('editMode');
+
   if (containsClass) {
     label.innerText = input.value;
     editBtn.className = 'material-icons edit';
     editBtn.innerHTML = '<i class="material-icons">edit</i>';
+
   } else {
     input.value = label.innerText;
     editBtn.className = 'material-icons save';
     editBtn.innerHTML = '<i class="material-icons">save</i>';
 
   }
+
   listItem.classList.toggle('editMode');
 }
 
@@ -88,18 +102,8 @@ function finishTask() {
   checkBox.innerHTML = '<i class="material-icons">check_box</i>'
 
 }
-/*function dragStart() {
-  this.style.color = 'red';
-}
-let listItem = document.querySelectorAll('li');
-[].forEach.call(listItem, function(el) {
-  el.addEventListener('dragstart', dragStart, false);
-});
-*/
-let dragSrcEl = null;
 
 function handleDragStart(e) {
-  // Target (this) element is the source node.
   dragSrcEl = this;
 
   e.dataTransfer.effectAllowed = 'move';
@@ -110,40 +114,30 @@ function handleDragStart(e) {
 
 function handleDragOver(e) {
   if (e.preventDefault) {
-    e.preventDefault(); // Necessary. Allows us to drop.
+    e.preventDefault();
   }
   this.classList.add('over');
 
-  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+  e.dataTransfer.dropEffect = 'move';
 
   return false;
 }
 
-function handleDragEnter(e) {
-  // this / e.target is the current hover target.
-}
-
-function handleDragLeave(e) {
-  this.classList.remove('over');  // this / e.target is previous target element.
+function handleDragLeave() {
+  this.classList.remove('over');
 }
 
 function handleDrop(e) {
-  // this/e.target is current target element.
 
   if (e.stopPropagation) {
-    e.stopPropagation(); // Stops some browsers from redirecting.
+    e.stopPropagation();
   }
 
-  // Don't do anything if dropping the same column we're dragging.
-  if (dragSrcEl != this) {
-    // Set the source column's HTML to the HTML of the column we dropped on.
-    //alert(this.outerHTML);
-    //dragSrcEl.innerHTML = this.innerHTML;
-    //this.innerHTML = e.dataTransfer.getData('text/html');
+  if (dragSrcEl !== this) {
     this.parentNode.removeChild(dragSrcEl);
-    var dropHTML = e.dataTransfer.getData('text/html');
+    let dropHTML = e.dataTransfer.getData('text/html');
     this.insertAdjacentHTML('beforebegin',dropHTML);
-    var dropElem = this.previousSibling;
+    let dropElem = this.previousSibling;
     addDnDHandlers(dropElem);
     
   }
@@ -151,25 +145,14 @@ function handleDrop(e) {
   return false;
 }
 
-function handleDragEnd(e) {
-  // this/e.target is the source node.
+function handleDragEnd() {
   this.classList.remove('over');
-
-  /*[].forEach.call(cols, function (col) {
-    col.classList.remove('over');
-  });*/
 }
 
 function addDnDHandlers(elem) {
   elem.addEventListener('dragstart', handleDragStart, false);
-  elem.addEventListener('dragenter', handleDragEnter, false)
   elem.addEventListener('dragover', handleDragOver, false);
   elem.addEventListener('dragleave', handleDragLeave, false);
   elem.addEventListener('drop', handleDrop, false);
   elem.addEventListener('dragend', handleDragEnd, false);
-
 }
-
-let cols = document.querySelectorAll('li');
-[].forEach.call(cols, addDnDHandlers);
-console.log(cols);
